d3ab655d905f5ae06d830b36a47467802d27e8a5
Hugo Des <hugo.deslongchamps@gmail.com>
2013-08-24 14:09:16 +0200

    le blop bouge !

--- main.lua
+++ main.lua
@@ -1,7 +1,28 @@
 function love.load()
 	player = love.graphics.newImage("images/characters/Pj_Blop.png")
+	x = 50
+	y = 50
+	speed = 300
+end
+
+function love.update(dt)
+	if love.keyboard.isDown("right") then
+	x = x + (speed * dt)
+	end
+
+	if love.keyboard.isDown("left") then
+	x = x -(speed*dt)
+	end
+
+	if love.keyboard.isDown("up") then
+	y = y -(speed *dt)
+	end
+
+	if love.keyboard.isDown("down") then
+	y = y +(speed*dt)
+	end
 end
 
 function love.draw()
-	love.graphics.draw(player, 0, 0)
+	love.graphics.draw(player, x, y, 0, 0.25, 0.25, 130, 130)
 end
