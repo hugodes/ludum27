8eea108fcaae11536352171fb7afd1ae733f89f7
Hugo Des <hugo.deslongchamps@gmail.com>
2013-08-24 15:57:43 +0200

    working timer

--- main.lua
+++ main.lua
@@ -3,9 +3,15 @@ function love.load()
 	x = 50
 	y = 50
 	speed = 300
+	stime = love.timer.getTime()
+	timer=0
 end
 
 function love.update(dt)
+	ntime = love.timer.getTime()
+	timer = 10-(ntime-stime)
+	
+
 	if love.keyboard.isDown("right") then
 	x = x + (speed * dt)
 	end
@@ -24,5 +30,6 @@ function love.update(dt)
 end
 
 function love.draw()
+	love.graphics.print(timer, 0, 0)
 	love.graphics.draw(player, x, y, 0, 0.25, 0.25, 130, 130)
 end
