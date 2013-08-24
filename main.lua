0545cb6d332fcae6fc8cae1aa3a72e73ae0bb6a1
Hugo Des <hugo.deslongchamps@gmail.com>
2013-08-24 13:32:25 +0200

    on voit le blop !

--- main.lua
+++ main.lua
@@ -1,3 +1,7 @@
+function love.load()
+	player = love.graphics.newImage("images/characters/Pj_Blop.png")
+end
+
 function love.draw()
-	love.graphics.print("Hello, world !", 400, 300)
+	love.graphics.draw(player, 0, 0)
 end
