8918e6f473af48172eee0c4fb8b47cecc5ca72fe
Hugo Des <hugo.deslongchamps@gmail.com>
2013-08-25 12:39:22 +0200

    wow it's starting to look like something

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+require("useful")
+
 function love.load()
 	screen_w = 800
 	screen_h = 600
@@ -9,28 +11,47 @@ function love.load()
 	player_size=0.125
 	stime = love.timer.getTime()
 	timer=0
+	timer_offset=0
 	
 	--loading our pnj images
 	pnj = {}
 	pnj[0]=nil -- pas de pnj
 	pnj[1]=love.graphics.newImage("images/pnj/Good_Blop.png")
 	pnj[2]=love.graphics.newImage("images/pnj/Bad_Blop.png")
-	--pnj positions
+	pnj[3]=love.graphics.newImage("images/pnj/Time_Blop.png")
+	--pnj positions and hit boxes
 	pnj_pos = {}
 	for i=0, 9 do
 		pnj_pos[i]={}
 		for j=0, 7 do
-			pnj_pos[i][j]=math.random(3)-1
+			pnj_pos[i][j]={}
+			random_number=math.random(4)-1
+			if random_number>0 then
+				pnj_pos[i][j]["type"]=random_number
+				if random_number == 1 then
+					pnj_pos[i][j]["pos_x"]=80*i
+					pnj_pos[i][j]["pos_y"]=80*j+22
+					pnj_pos[i][j]["width"]=80
+					pnj_pos[i][j]["height"]=35
+				elseif random_number == 2 then
+					pnj_pos[i][j]["pos_x"]=80*i+50
+					pnj_pos[i][j]["pos_y"]=80*j+40
+					pnj_pos[i][j]["radius"]=20
+				elseif random_number == 3 then
+					pnj_pos[i][j]["pos_x"]=80*i+40
+					pnj_pos[i][j]["pos_y"]=80*j+40
+					pnj_pos[i][j]["radius"]=20
+				end
+			end
 		end
 	end
-			
-	
-	
+	--position de départ du joueur
+	pnj_pos[5][4]["type"]=0
 end
 
 function love.update(dt)
 	ntime = love.timer.getTime()
-	timer = 10-(ntime-stime)
+	timer = 10-(ntime-stime)+timer_offset
 	
 
 	if love.keyboard.isDown("right") then
@@ -68,20 +89,47 @@ function love.update(dt)
 			player_y=new_pos_y
 		end
 	end
+	player_hb={}
+	player_hb["pos_x"]=player_x
+	player_hb["pos_y"]=player_y
+	player_hb["radius"]=130*player_size
 	
-	player_grid_x = math.floor(player_x / 80)
-	player_grid_y = math.floor(player_y /80)
-
-	if pnj_pos[player_grid_x][player_grid_y] == 1 then
-		pnj_pos[player_grid_x][player_grid_y]=0
-		player_size = player_size + player_size*0.2
+	for i=0,9 do
+		for j=0,6 do
+			if pnj_pos[i][j]["type"]==1 then
+				if useful.col_circle_rectangle(player_hb["pos_x"],
+				player_hb["pos_y"],
+				player_hb["radius"],
+				pnj_pos[i][j]["pos_x"],
+				pnj_pos[i][j]["pos_y"],
+				pnj_pos[i][j]["width"],
+				pnj_pos[i][j]["height"]) then
+					pnj_pos[i][j]["type"]=0
+					player_size = player_size + player_size*0.2
+				end
+			elseif pnj_pos[i][j]["type"]==2 then
+				if useful.col_circle_circle(player_hb["pos_x"],
+				player_hb["pos_y"],
+				player_hb["radius"],
+				pnj_pos[i][j]["pos_x"],
+				pnj_pos[i][j]["pos_y"],
+				pnj_pos[i][j]["radius"]) then
+					pnj_pos[i][j]["type"]=0
+					player_size = player_size -player_size*0.2
+				end
+			elseif pnj_pos[i][j]["type"]==3 then
+				if useful.col_circle_circle(player_hb["pos_x"],
+				player_hb["pos_y"],
+				player_hb["radius"],
+				pnj_pos[i][j]["pos_x"],
+				pnj_pos[i][j]["pos_y"],
+				pnj_pos[i][j]["radius"]) then
+					pnj_pos[i][j]["type"]=0
+					timer_offset=timer_offset+0.5
+				end
+			end
+		end
 	end
-	if pnj_pos[player_grid_x][player_grid_y] == 2 then
-		pnj_pos[player_grid_x][player_grid_y]=0
-		player_size = player_size - player_size*0.2
-	end
-		
-	
 end
 
 function love.draw()
@@ -90,8 +138,22 @@ function love.draw()
 	for i=0,9 do
 		for j=0,6 do
 			--print(pnj_pos[i][j])
-			if pnj[pnj_pos[i][j]] and (i~=5 and j~=4) then
-				love.graphics.draw(pnj[pnj_pos[i][j]], i*80, j*80)
+			if pnj[pnj_pos[i][j]["type"]] then
+				love.graphics.draw(pnj[pnj_pos[i][j]["type"]], i*80, j*80)
+				if pnj_pos[i][j]["type"] == 1 then
+					love.graphics.rectangle("line",
+					pnj_pos[i][j]["pos_x"],
+					pnj_pos[i][j]["pos_y"],
+					pnj_pos[i][j]["width"],
+					pnj_pos[i][j]["height"])
+				else
+					love.graphics.circle("line",pnj_pos[i][j]["pos_x"],
+					pnj_pos[i][j]["pos_y"],
+					pnj_pos[i][j]["radius"])
+				end
+				love.graphics.circle("line",player_hb["pos_x"],
+				player_hb["pos_y"],
+				player_hb["radius"])
 			end
 		end
 	end
